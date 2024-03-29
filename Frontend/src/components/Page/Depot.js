8653b752db76366f413f12283bf79603100fb869
Salim <lakhalsalim@hotmail.com>
2024-03-30 00:10:32 +0100

    Ajout d'un emplacement drag and drop dans la page depot de fichier pour récuperer les fichiers

--- Frontend/src/components/Page/Depot.js
+++ Frontend/src/components/Page/Depot.js
@@ -1,27 +1,98 @@
-
-import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import './depot.css' ;
-import Footer from '../Footer/Footer';
+import React, { useEffect } from 'react';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import './depot.css';
 
 const Depot = () => {
+  useEffect(() => {
+    document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+      const dropZoneElement = inputElement.closest(".drop-zone");
+
+      dropZoneElement.addEventListener("click", (e) => {
+        inputElement.click();
+      });
+
+      inputElement.addEventListener("change", (e) => {
+        if (inputElement.files.length) {
+          updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+      });
+
+      dropZoneElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+      });
+
+      ["dragleave", "dragend"].forEach((type) => {
+        dropZoneElement.addEventListener(type, (e) => {
+          dropZoneElement.classList.remove("drop-zone--over");
+        });
+      });
+
+      dropZoneElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+          inputElement.files = e.dataTransfer.files;
+          updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  }, []);
+
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+      dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
+    } else {
+      thumbnailElement.style.backgroundImage = null;
+    }
+  }
 
   return (
-    <div>
-      < Header /> 
-    <div className="botStyle">
-      
-      {/* Speech Bubble */}
-      {/*<div className="bubbleStyle">
-        Voici notre sytème de stockage decentralisé, vous pouvez deposer et retirer vos fichier en toute sécurité.
-      </div>*/}
-      
-      {/* KryptoBot Logo */}
-      <img className="logoStyle" src={"logopote.png"} alt="KryptoBot" />
-      < Footer /> 
-
-    </div>
+    <div > 
+      <Header />
+      <div className="depot-container"> 
+      <div className="botStyle1">
+        {/* KryptoBot Logo */}
+        <img className="logoStyle1" src="logopote.png" alt="KryptoBot" />
+      </div>
+        {/* Zone de dépôt de fichier */}
+        <div className="drop-zone">
+          <span className="drop-zone__prompt">Drop file here or click to upload</span>
+          <input type="file" name="myFile" className="drop-zone__input" />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
