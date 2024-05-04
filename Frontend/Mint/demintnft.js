ede96d8e9774284cdcb5c4bf3da34908fd7b3e0a
Amine <amine.hedroug@telecom-sudparis.eu>
2024-05-04 16:00:05 +0200

    mesfichierdesign

--- Frontend/Mint/demintnft.js
+++ Frontend/Mint/demintnft.js
@@ -1,25 +1,32 @@
 async function getTokens() {
-
+    
     const standby_wallet = xrpl.Wallet.fromSeed(standbySeedField.value);
-    let net = getNet();
+    const net = getNet();
     const client = new xrpl.Client(net);
     let results = 'Connecting to ' + net + '...';
     standbyResultField.value = results;
-    await client.connect();
-    results += '\nConnected. Getting NFTs...';
-    standbyResultField.value = results;
+    
+    try {
+        await client.connect();
+        results += '\nConnected. Getting NFTs...';
+        standbyResultField.value = results;
 
-    const nfts = await client.request({
-        method: "account_nfts",
-        account: standby_wallet.classicAddress
-    });
+        const nfts = await client.request({
+            method: "account_nfts",
+            account: standby_wallet.classicAddress
+        });
+        
+        const uris = nfts.map(getNFTuri);
+        return uris;
 
-    const selectedNFT = nfts[0];
+    } catch (error) {
+        console.error("Error getting NFTs:", error);
+        return [];
+    } finally {
+        client.disconnect();
+    }
+}
 
-    const selectedNFTURI = selectedNFT.uri;
-
-    results += '\nURI du NFT sélectionné: ' + selectedNFTURI;
-    standbyResultField.value = results;
-
-    client.disconnect();
+async function getNFTuri(nft) {
+    return convertHexToString(nft.uri);
 }
